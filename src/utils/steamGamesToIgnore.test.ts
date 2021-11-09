46fa9f885d63291e115d4152411e2c49ce525eaa
Qwx <qwxmail@gmail.com>
2021-11-09 22:53:13 +0200

    Started work on games ignore

--- src/utils/steamGamesToIgnore.test.ts
+++ src/utils/steamGamesToIgnore.test.ts
@@ -1,7 +1,13 @@
+import { game } from "../types/game";
+import { testGame } from '../../test_utils/testData';
 import { steamGamesToIgnore } from "./steamGamesToIgnore";
 
 describe('steamGamesToIgnore', () => {
-    it('hmm', () => {
-        const gamesToIgnore = steamGamesToIgnore([]);
+    it('skips games owned on steam', () => {
+        const games: game[] = [{ ...testGame, title: 'Steam Game', releaseKeys: ['steam_123'], releases: ['steam_123'] }];
+
+        const gamesToIgnore = steamGamesToIgnore(games);
+
+        expect(gamesToIgnore.length).toBe(0);
     })
 })
---
+++ test_utils/testData.ts
@@ -0,0 +1,18 @@
+import { game } from "../src/types/game";
+
+export const testGame: game = {
+    title: 'Test Game',
+    backgroundImage: '',
+    criticsScore: null,
+    developers: [],
+    gameMinutes: 0,
+    genres: [],
+    publishers: [],
+    releaseDate: null,
+    releaseKeys: ['abc_abc'],
+    squareIcon: '',
+    summary: '',
+    themes: [],
+    verticalCover: '',
+    releases: ['abc_abc'],
+};
